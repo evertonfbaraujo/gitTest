9fe1fe9c71fd97411da85a760d32c904f94e9bf3
Everton Araujo <evertonfilipe.araujo@gmail.com>
2021-10-21 13:12:49 -0300

    adicionado GSAP e PixiJS

--- js/index.js
+++ js/index.js
@@ -16,20 +16,18 @@ var canvasWidth =  document.querySelector('.container').offsetWidth
 var canvasHeight =  document.querySelector('.container').offsetHeight
 canvas.width = canvasWidth
 canvas.height = canvasHeight
+let tFlower = new Image()
+let bFlower = new Image()
+
+tFlower.onload = function(){
+    ctx.drawImage(tFlower, -100 ,-150 , 570, 460)
+}
+tFlower.src = "../imgs/top-right-flowers.svg"
 
 
-// let tFlower = new Image()
-// let bFlower = new Image()
+bFlower.onload = ()=>{
+    ctx.drawImage(bFlower, -90, 640 , 459,492)
+}
 
-// tFlower.onload = function(){
-//     ctx.drawImage(tFlower, -100 ,-150 , 570, 460)
-// }
-// tFlower.src = "../imgs/top-right-flowers.svg"
-
-
-// bFlower.onload = ()=>{
-//     ctx.drawImage(bFlower, -90, 640 , 459,492)
-// }
-
-// bFlower.src = "../imgs/bottom-left-flowers.svg"
+bFlower.src = "../imgs/bottom-left-flowers.svg"
 

3432a0f03ffdef310785fe85dcc2c866d61a3b62
Everton UnderScore <wd.evertonaraujo@gmail.com>
2021-10-25 11:37:10 -0300

    adicionado a flor do fundo

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var canvasWidth = document.querySelector(".container").offsetWidth;
 var canvasHeight = document.querySelector(".container").offsetHeight;
 const app = new PIXI.Application({
   width: canvasWidth,
-  height: canvasHeight,
+  height: canvasHeight + 10,
   backgroundColor: 0x1099bb,
 });
 document.getElementById("container").appendChild(app.view);
@@ -13,31 +13,41 @@ app.stage.addChild(container);
 
 // Create a new texture
 const texture = [
- tf = PIXI.Texture.from("../imgs/top-right-flowers.svg"),
- wf = PIXI.Texture.from("../imgs/little-flower.svg")
-]
+  // Top Flowers
+  (tf = PIXI.Texture.from("../imgs/top-right-flowers.svg")),
+  //Little white Flower
+  (lwf = PIXI.Texture.from("../imgs/little-flower.svg")),
+  //Big Flower side left
+   (lsf = PIXI.Texture.from("../imgs/flower-left-side.png")),
+  //Bottom Flowers
+  (bf = PIXI.Texture.from("../imgs/bottom-left-flowers.svg")),
+];
 // const texture = ;
 
 const tF = new PIXI.Sprite(texture[0]);
 tF.anchor.set(0.5);
 tF.x = 190;
 tF.y = 50;
-container.addChild(tF);
 
-let fls = new PIXI.Sprite(PIXI.Texture.from("../imgs/flower-left-side.png"))
-fls.x = 0
-fls.y = 380
-fls.anchor.set(0.5)
+
+let fls = new PIXI.Sprite(texture[2]);
+fls.x = 0;
+fls.y = 380;
+fls.anchor.set(0.5);
 
 let florzinha = new PIXI.Sprite(texture[1]);
-florzinha.anchor.set(0.5)
+florzinha.anchor.set(0.5);
 florzinha.x = 100;
 florzinha.y = 500;
 
 
-container.addChild(florzinha, fls);
+let blf =  new PIXI.Sprite(texture[3])
+blf.y =  parseInt(canvasHeight*68/100)
+blf.x =  -90
+
+
+container.addChild(tF,florzinha, fls,blf);
 
 app.ticker.add((delta) => {
-
   florzinha.rotation += 0.02 * delta;
 });

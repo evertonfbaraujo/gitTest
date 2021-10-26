9547f23701e1c7dc78808b5b7808a587bd400b28
Everton Araujo <evertonaraujo@pop-os.localdomain>
2021-10-26 18:15:44 -0300

    Alguns ajustes do css,html e js

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ const texture = [
   //Little white Flower
   (lwf = PIXI.Texture.from("../imgs/little-flower.svg")),
   //Big Flower side left
-   (lsf = PIXI.Texture.from("../imgs/flower-left-side.png")),
+  (lsf = PIXI.Texture.from("../imgs/flower-left-side.png")),
   //Bottom Flowers
   (bf = PIXI.Texture.from("../imgs/bottom-left-flowers.svg")),
   //Brown Flower
@@ -31,7 +31,6 @@ tF.anchor.set(0.5);
 tF.x = 190;
 tF.y = 50;
 
-
 let fls = new PIXI.Sprite(texture[2]);
 fls.x = 0;
 fls.y = 380;
@@ -42,18 +41,16 @@ florzinha.anchor.set(0.5);
 florzinha.x = 100;
 florzinha.y = 500;
 
+let blf = new PIXI.Sprite(texture[3]);
+blf.y = parseInt((canvasHeight * 65) / 100);
+blf.x = -90;
 
-let blf =  new PIXI.Sprite(texture[3])
-blf.y =  parseInt(canvasHeight*68/100)
-blf.x =  -90
+let lbf1 = new PIXI.Sprite(texture[4]);
+lbf1.x = 100;
+lbf1.y = parseInt(canvasHeight / 2);
+lbf1.anchor.set(0.5);
 
-let lbf1 = new PIXI.Sprite(texture[4])
-lbf1.x = 100
-lbf1.y = parseInt(canvasHeight/2)
-lbf1.anchor.set(0.5)
-
-
-container.addChild(tF,florzinha,fls,blf,lbf1);
+container.addChild(tF, florzinha, fls, blf, lbf1);
 
 app.ticker.add((delta) => {
   florzinha.rotation += 0.02 * delta;

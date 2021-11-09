389d75e1cc6ea3915c5aec28dd1baae13818caea
Everton Araujo <evertonfilipe.araujo@gmail.com>
2021-11-09 01:16:33 -0300

    configurações de alerta e animação de bg

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 var canvasWidth = document.querySelector(".container").offsetWidth;
 var canvasHeight = document.querySelector(".container").offsetHeight;
+const PAD = 80;
+const resolution = 1;
+const WIDTH = canvasWidth / resolution;
+const HEIGHT = canvasHeight / resolution;
+
 const app = new PIXI.Application({
   width: canvasWidth,
   height: canvasHeight + 10,
@@ -11,46 +16,110 @@ const container = new PIXI.Container();
 
 app.stage.addChild(container);
 
-const texture = [
-  // Top Flowers
-  (tf = PIXI.Texture.from("../imgs/top-right-flowers.svg")),
-  //Little white Flower
-  (lwf = PIXI.Texture.from("../imgs/little-flower.svg")),
-  //Big Flower side left
-  (lsf = PIXI.Texture.from("../imgs/flower-left-side.png")),
-  //Bottom Flowers
-  (bf = PIXI.Texture.from("../imgs/bottom-left-flowers.svg")),
-  //Brown Flower
-  (lbf = PIXI.Texture.from("../imgs/little-brown-flower.svg")),
-];
-// const texture = ;
+const bunnyTexture = PIXI.Texture.from("../imgs/little-flower.svg");
+function updateBunny(bunny) {
+  bunny.x += bunny.vx;
+  bunny.y += bunny.vy;
+  if (bunny.x > WIDTH + PAD) {
+    bunny.x -= WIDTH + 2 * PAD;
+  }
+  if (bunny.x < -PAD) {
+    bunny.x += WIDTH + 2 * PAD;
+  }
+  if (bunny.y > HEIGHT + PAD) {
+    bunny.y -= HEIGHT + 2 * PAD;
+  }
+  if (bunny.y < -PAD) {
+    bunny.y += HEIGHT + 2 * PAD;
+  }
+}
 
-const tF = new PIXI.Sprite(texture[0]);
-tF.anchor.set(0.5);
-tF.x = 190;
-tF.y = 50;
+function createBunny() {
+  const bunny = new PIXI.Sprite(bunnyTexture);
+  bunny.update = updateBunny;
 
-let fls = new PIXI.Sprite(texture[2]);
-fls.x = 0;
-fls.y = 380;
-fls.anchor.set(0.5);
+  const angle = Math.random() * Math.PI * 2;
+  const speed = 50.0; // px per second
+  bunny.vx = (Math.cos(angle) * speed) / 60.0;
+  bunny.vy = (Math.sin(angle) * speed) / 60.0;
+  bunny.position.set(Math.random() * WIDTH, Math.random() * HEIGHT);
+  bunny.anchor.set(0.5, 0.5);
 
-let florzinha = new PIXI.Sprite(texture[1]);
-florzinha.anchor.set(0.5);
-florzinha.x = 100;
-florzinha.y = 500;
+  return bunny;
+}
 
-let blf = new PIXI.Sprite(texture[3]);
-blf.y = parseInt((canvasHeight * 65) / 100);
-blf.x = -90;
+for (let i = 0; i < 10; i++) {
+  container.addChild(createBunny());
+}
 
-let lbf1 = new PIXI.Sprite(texture[4]);
-lbf1.x = 100;
-lbf1.y = parseInt(canvasHeight / 2);
-lbf1.anchor.set(0.5);
-
-container.addChild(tF, florzinha, fls, blf, lbf1);
-
-app.ticker.add((delta) => {
-  florzinha.rotation += 0.02 * delta;
+app.ticker.add(() => {
+  container.children.forEach(updateBunny);
 });
+
+// //load textures
+// const texture = [
+//   // Top Flowers
+//   (tf = PIXI.Texture.from("../imgs/top-right-flowers.svg")),
+//   //Little white Flower
+//   (lwf = PIXI.Texture.from("../imgs/little-flower.svg")),
+//   //Big Flower side left
+//   (lsf = PIXI.Texture.from("../imgs/flower-left-side.png")),
+//   //Bottom Flowers
+//   (bf = PIXI.Texture.from("../imgs/bottom-left-flowers.svg")),
+//   //Brown Flower
+//   (lbf = PIXI.Texture.from("../imgs/little-brown-flower.svg")),
+// ];
+
+// const tF = new PIXI.Sprite(texture[0]);
+// tF.anchor.set(0.5);
+// tF.x = 190;
+// tF.y = 50;
+
+// //WhiteFlowers
+// let florzinha = new PIXI.Sprite(texture[1]);
+// florzinha.anchor.set(0.5);
+// florzinha.x = 100;
+// florzinha.y = 500;
+// container.addChild(florzinha);
+
+// let florzinha1 = new PIXI.Sprite(texture[1]);
+// florzinha.anchor.set(0.5);
+// florzinha.x = 300;
+// florzinha.y = 500;
+// container.addChild(florzinha1);
+
+// let fls = new PIXI.Sprite(texture[2]);
+// fls.x = 0;
+// fls.y = 380;
+// fls.anchor.set(0.5);
+
+// let blf = new PIXI.Sprite(texture[3]);
+// blf.y = parseInt((canvasHeight * 65) / 100);
+// blf.x = -90;
+
+// let lbf1 = new PIXI.Sprite(texture[4]);
+// lbf1.x = 100;
+// lbf1.y = parseInt(canvasHeight / 2);
+// lbf1.anchor.set(0.5);
+
+// container.addChild(tF, florzinha, fls, blf, lbf1);
+
+// // app.ticker.add((delta) => {
+// //   florzinha.rotation += 0.02 * delta;
+// // });
+
+/* alerta quadro */
+var quadro = document.querySelector(".quadro");
+var heightalerta = parseInt(document.querySelector(".alerta").offsetHeight / 2);
+var widthtalerta = parseInt(document.querySelector(".alerta").offsetWidth / 2);
+var hAltQuadro = parseInt(quadro.scrollHeight / 2);
+var wAltQuadro = parseInt(quadro.clientWidth / 2);
+quadro.style.top = heightalerta - hAltQuadro + "px";
+quadro.style.left = widthtalerta - wAltQuadro + "px";
+
+var modal = document.querySelector(".alerta");
+var closeBtn = document.querySelector(".close");
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+};
